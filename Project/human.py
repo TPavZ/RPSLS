33feb7ab0ffae0e347dd8f7f895a5bc88e71d8a2
Joshua-Dela-Pena <richmelldelapena@gmail.com>
2021-11-16 14:24:53 -0600

    Cleaned some functions to simplify it a bit more and make it less convoluted

---
+++ Project/human.py
@@ -0,0 +1,12 @@
+from player import Player
+
+class Human(Player): #need to make a gesture choice
+    
+    def __init__(self, player):
+        self.name = player
+        super().__init__(player)
+        pass
+        
+    
+
+        
