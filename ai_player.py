fe91f8edbcd008ca6329ef9c03149cdaa271ba91
Joshua-Dela-Pena <richmelldelapena@gmail.com>
2021-11-16 12:08:24 -0600

    I created a random choice function for the AI's hand gesture within my AI class that pulls a random string from a list

---
+++ ai_player.py
@@ -0,0 +1,19 @@
+import random
+
+class AI:
+    def __init__(self, name):
+        self.name = name
+        super().__init__(name)
+        
+    def hand_gesture(self):
+        self.gesture = random.choice()
+        if self.gesture == "Rock":
+            print(f'Player 2 selected {self.gesture}')
+        elif self.gesture == "Paper":
+            print(f'Player 2 selected {self.gesture}')
+        elif self.gesture == "Scissors":
+            print(f'Player 2 selected {self.gesture}')
+        elif self.gesture == "Lizard":
+            print(f'Player 2 selected {self.gesture}')
+        elif self.gesture == "Spock":
+            print(f'Player 2 selected {self.gesture}')
